790c269b286ebdd556701107c6db65075b2ee861
대구핫플여기 <ysh5286@gmail.com>
2026-02-10 09:51:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dazzle-map-v3.1'; // 버전 업데이트 시 같이수정 >> index >> navigator.serviceWorker.register('./sw.js?v=3.1') 
+const CACHE_NAME = 'dazzle-map-v3.2'; // 버전 업데이트 시 같이수정 >> index >> navigator.serviceWorker.register('./sw.js?v=3.1') 
 
 const urlsToCache = [
   './',
